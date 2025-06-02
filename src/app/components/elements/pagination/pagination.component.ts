9376817b653e33ae776de481a55b8a66e57c2f56
Jakub Bryska <troppout@gmail.com>
2025-06-02 12:54:22 +0200

    add pagination component and few styles

---
+++ src/app/components/elements/pagination/pagination.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css',
+})
+export class PaginationComponent {
+  @Input() startIndex?: number;
+  @Input() endIndex?: number;
+  @Input() currentPage?: number;
+  @Input() maxPage?: number;
+  @Input() elementsCount?: number;
+  @Output() changePage = new EventEmitter<string>();
+}
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../services/interfaces/products.interface';
 import { ProductCardComponent } from '../../components/elements/product-card/product-card.component';
@@ -7,6 +7,7 @@ import { BannerComponent } from '../../components/elements/banner/banner.compone
 import { CartService } from '../../services/cart.service';
 import { PopupService } from '../../services/popup.service';
 import { ProductCardSkeletonComponent } from '../../components/elements/product-card-skeleton/product-card-skeleton.component';
+import { PaginationComponent } from '../../components/elements/pagination/pagination.component';
 
 @Component({
   selector: 'app-products',
@@ -15,6 +16,7 @@ import { ProductCardSkeletonComponent } from '../../components/elements/product-
     ProductCardComponent,
     BannerComponent,
     ProductCardSkeletonComponent,
+    PaginationComponent,
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
@@ -25,7 +27,7 @@ export class ProductsComponent {
   private cartService = inject(CartService);
   private router = inject(Router);
 
-  skeletonArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  skeletonArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   products: Product[] | null = [];
   error: string = '';
   loadingProducts: boolean = false;
@@ -44,6 +46,9 @@ export class ProductsComponent {
         if (data.length < 1) {
           this.error = 'No products found.';
         }
+        this.maxPages = Math.ceil(data.length / this.limit);
+        this.currentPage = 1;
+        this.updateShownProducts();
         console.log(this.products);
       },
       error: (err) => {
@@ -64,5 +69,38 @@ export class ProductsComponent {
 
   openDetails(id: number) {
     this.router.navigate(['/products', id]);
+  }
+
+  maxPages: number = 0;
+  currentPage: number = 1;
+  limit: number = 9;
+  showProducts: Product[] | null = [];
+
+  changePage(action: string) {
+    if (action === 'prev' && this.currentPage > 1) {
+      this.currentPage--;
+      this.updateShownProducts();
+    } else if (action === 'next' && this.currentPage < this.maxPages) {
+      this.currentPage++;
+      this.updateShownProducts();
+    }
+    setTimeout(() => {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'auto' });
+    }, 0);
+  }
+
+  startIndex: number = 0;
+  endIndex: number = 0;
+  updateShownProducts() {
+    if (!this.products) return;
+    const startIndex = (this.currentPage - 1) * this.limit;
+    const endIndex = startIndex + this.limit;
+    if (endIndex >= this.products.length) {
+      this.endIndex = this.products.length;
+    } else {
+      this.endIndex = endIndex;
+    }
+    this.startIndex = startIndex + 1;
+    this.showProducts = this.products.slice(startIndex, endIndex);
   }
 }
