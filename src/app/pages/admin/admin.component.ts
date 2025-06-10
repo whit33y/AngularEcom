d6e489c381820b58bf273a9b97c1509f01bd2956
Jakub Bryska <troppout@gmail.com>
2025-06-10 11:58:47 +0200

    add admin routes

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AdminProductsComponent, AdminUsersComponent],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
 export class AdminComponent {
+  selectedRoute = 'users';
 
+  changeRoute(route: string) {
+    this.selectedRoute = route;
+    console.log(this.selectedRoute);
+  }
 }
