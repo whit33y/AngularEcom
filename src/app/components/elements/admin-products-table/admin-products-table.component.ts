b044c2abea376868bf3670fa217fdbb0260ba9a1
Jakub Bryska <troppout@gmail.com>
2025-06-12 15:08:28 +0200

    change styles on admin tables

--- src/app/components/elements/admin-products-table/admin-products-table.component.ts
+++ src/app/components/elements/admin-products-table/admin-products-table.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../services/interfaces/products.interface';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-admin-products-table',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe],
   templateUrl: './admin-products-table.component.html',
   styleUrl: './admin-products-table.component.css',
 })
