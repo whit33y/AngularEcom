c604ec90010e07fb0d6cfea403641ef98164fc98
Jakub Bryska <troppout@gmail.com>
2025-06-13 12:29:39 +0200

    add form component

--- src/app/components/elements/admin-products-form/admin-products-form.component.ts
+++ src/app/components/elements/admin-products-form/admin-products-form.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-products-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-products-form.component.html',
-  styleUrl: './admin-products-form.component.css'
+  styleUrl: './admin-products-form.component.css',
 })
 export class AdminProductsFormComponent {
-
+  productForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    price: new FormControl(0, [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+  });
 }
